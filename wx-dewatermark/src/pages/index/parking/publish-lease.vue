301e0c02563fde5b8a71f944bbc50be4e35d64c5
zhulixiao <1280253718@qq.com>
2023-11-21 00:23:55 +0800

    微调

--- wx-dewatermark/src/pages/index/parking/publish-lease.vue
+++ wx-dewatermark/src/pages/index/parking/publish-lease.vue
@@ -6,10 +6,10 @@
       </view>
     </uni-card>
     <uni-card :is-shadow="false">
-      <uni-easyinput :inputBorder="false" type="textarea" v-model="baseFormData.introduction"
+      <uni-easyinput :inputBorder="false" type="textarea" v-model="baseFormData.title"
         placeholder="请输入车位详细介绍（例如：车位在几号楼下哪个区）" />
       <view>
-        <uni-file-picker v-model="imageValue" file-mediatype="image" mode="grid" file-extname="png,jpg" :limit="1"
+        <uni-file-picker v-model="imageValue.valueOf" file-mediatype="image" mode="grid" file-extname="png,jpg" :limit="1"
           @progress="progress" @success="success" @fail="fail" @select="select" />
       </view>
     </uni-card>
@@ -17,22 +17,22 @@
       <uni-forms ref="baseForm" :modelValue="baseFormData" label-position="left">
 
         <uni-forms-item label="租金" required>
-          <uni-data-select v-model="seleIndex" :localdata="range" @change="onchange" :clear="false"
+          <uni-data-select v-model="baseFormData.annual_rent" :localdata="seleAry3" @change="onchange" :clear="false"
             placeholder="请选择"></uni-data-select>
         </uni-forms-item>
         <uni-forms-item label="是否包含理费" required>
-          <uni-data-checkbox v-model="baseFormData.sex" :localdata="sexs" />
+          <uni-data-checkbox v-model="baseFormData.in_maintenance" :localdata="seleAry1" />
         </uni-forms-item>
 
         <uni-forms-item label="价格能否协商" required>
-          <uni-data-checkbox v-model="baseFormData.hobby" :localdata="hobbys" />
+          <uni-data-checkbox v-model="baseFormData.negotiable" :localdata="seleAry2" />
         </uni-forms-item>
         <uni-forms-item label="手机号" required>
-          <uni-easyinput v-model="baseFormData.name" placeholder="请输入联系方式" />
+          <uni-easyinput v-model="baseFormData.telephone" placeholder="请输入联系方式" />
         </uni-forms-item>
 
         <uni-forms-item label="微信号">
-          <uni-easyinput v-model="baseFormData.name" placeholder="（选填项）可让租客加微信联系" />
+          <uni-easyinput v-model="baseFormData.wei_xin" placeholder="（选填项）可让租客加微信联系" />
         </uni-forms-item>
 
       </uni-forms>
@@ -48,18 +48,22 @@ import { RequestApi } from '@/public/request';
 import { onLoad } from '@dcloudio/uni-app';
 import { computed, ref } from 'vue';
 
+
 // 基础表单数据
 let baseFormData = {
-  name: '',
-  age: '',
-  introduction: '',
-  sex: 2,
-  hobby: [5],
-  datetimesingle: 1627529992399
+  title: '', // 
+  telephone: '', // 联系方式，对应后端的 Telephone
+  wei_xin: '', // 微信号
+  in_maintenance: false, // 是否在维护中，对应后端的 InMaintenance
+  negotiable: false, // 价格是否可协商，对应后端的 Negotiable
+  annual_rent: '', // 租金，对应后端的 AnnualRent
+  img_url: '',
+  // 其他字段根据后端模型添加
 };
 
+
 // 单选数据源
-let sexs = [{
+let seleAry1 = [{
   text: '包含',
   value: 0
 }, {
@@ -67,7 +71,7 @@ let sexs = [{
   value: 1
 }];
 // 多选数据源
-let hobbys = [{
+let seleAry2 = [{
   text: '可协商',
   value: 0
 }, {
@@ -80,7 +84,7 @@ const QiniuData = {    //这里是直接绑定data的值
   key: "", //图片名字处理
   token: "", //七牛云token
 }
-const range = [
+const seleAry3 = [
   { value: 0, text: "600元/年" },
   { value: 1, text: "700元/年" },
   { value: 2, text: "800元/年" },
@@ -136,10 +140,59 @@ onLoad(options => {
 
 });
 const handleItemClick = (itemModel: any) => {
-
-
+  if (!baseFormData.title) {
+    uni.showToast({
+      title: '请输入介绍',
+      icon: 'none',
+      duration: 2000,
+    });
+    return;
+  }
+  if (!baseFormData.telephone ) {
+    uni.showToast({
+      title: '请填写手机号',
+      icon: 'none',
+      duration: 2000,
+    });
+    return;
+  }
+  if (!baseFormData.annual_rent ) {
+    uni.showToast({
+      title: '请选择租金',
+      icon: 'none',
+      duration: 2000,
+    });
+    return;
+  }
+  publishLeasePosts();
 }
+// MARK: 
+async function publishLeasePosts() {
 
+  try {
+    const requestData = {
+      title: baseFormData.title,
+      telephone: baseFormData.telephone,
+      wei_xin: baseFormData.wei_xin,
+      in_maintenance: baseFormData.in_maintenance,
+      negotiable: baseFormData.negotiable,
+      posts_type: 0, // Modify this based on your data structure
+      state: 0, // Modify this based on your data structure
+      annual_rent: baseFormData.annual_rent,
+      // Add other fields based on your data structure
+    };
+    const res: any = await RequestApi.AddPosts(requestData)
+  
+    if (res.code === 200) {
+     
+    } else {
+      uni.showToast({ title: res.msg, icon: 'none', duration: 2000 })
+    }
+  } catch (error) {
+    console.error(error)
+    uni.showToast({ title: '请求失败', icon: 'none', duration: 2000 })
+  }
+}
 const onchange = (e: any) => {
   seleIndex.value = e;
   console.log("e:", e);
